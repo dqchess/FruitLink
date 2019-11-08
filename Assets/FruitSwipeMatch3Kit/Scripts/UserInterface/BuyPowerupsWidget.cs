64931ecca03aea04c965621bd64437ecae02c3c3
DungHA <>
2019-11-08 17:36:32 +0700

    Default item 5

--- Assets/FruitSwipeMatch3Kit/Scripts/UserInterface/BuyPowerupsWidget.cs
+++ Assets/FruitSwipeMatch3Kit/Scripts/UserInterface/BuyPowerupsWidget.cs
@@ -2,6 +2,7 @@
 // This code can only be used under the standard Unity Asset Store End User License Agreement,
 // a copy of which is available at http://unity3d.com/company/legal/as_terms.
 
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -23,12 +24,24 @@ namespace FruitSwipeMatch3Kit
         private List<PowerupButton> buttons;
 #pragma warning restore 649
 
+        private void Start()
+        {
+            if(!PlayerPrefs.HasKey("num_boosters_0"))
+                PlayerPrefs.SetInt("num_boosters_0", 5);
+            if(!PlayerPrefs.HasKey("num_boosters_1"))
+                PlayerPrefs.SetInt("num_boosters_1", 5);
+            if(!PlayerPrefs.HasKey("num_boosters_2"))
+                PlayerPrefs.SetInt("num_boosters_2", 5);
+            if(!PlayerPrefs.HasKey("num_boosters_3"))
+                PlayerPrefs.SetInt("num_boosters_3", 5);
+        }
+
         public void Initialize(LevelData levelData)
         {
-            buttons[0].Initialize(gameScreen, boosterSprites[0], PlayerPrefs.GetInt("num_boosters_0", 5), levelData.IsCrusherAvailable);
-            buttons[1].Initialize(gameScreen, boosterSprites[1], PlayerPrefs.GetInt("num_boosters_1", 5), levelData.IsBombAvailable);
-            buttons[2].Initialize(gameScreen, boosterSprites[2], PlayerPrefs.GetInt("num_boosters_2", 5), levelData.IsSwapAvailable);
-            buttons[3].Initialize(gameScreen, boosterSprites[3], PlayerPrefs.GetInt("num_boosters_3", 5), levelData.IsColorBombAvailable);
+            buttons[0].Initialize(gameScreen, boosterSprites[0], PlayerPrefs.GetInt("num_boosters_0"), levelData.IsCrusherAvailable);
+            buttons[1].Initialize(gameScreen, boosterSprites[1], PlayerPrefs.GetInt("num_boosters_1"), levelData.IsBombAvailable);
+            buttons[2].Initialize(gameScreen, boosterSprites[2], PlayerPrefs.GetInt("num_boosters_2"), levelData.IsSwapAvailable);
+            buttons[3].Initialize(gameScreen, boosterSprites[3], PlayerPrefs.GetInt("num_boosters_3"), levelData.IsColorBombAvailable);
         }
 
         public void PressButton(int index)
